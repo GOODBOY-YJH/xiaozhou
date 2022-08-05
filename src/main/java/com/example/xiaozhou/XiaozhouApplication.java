c10247b639ba48a890d6a1ec64ff1e9f08aa59a7
GOODBOY-YJH <1613777456@qq.com>
2022-08-05 09:41:07 +0800

    Initial commit

---
+++ src/main/java/com/example/xiaozhou/XiaozhouApplication.java
@@ -0,0 +1,13 @@
+package com.example.xiaozhou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaozhouApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaozhouApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xiaozhou/Inte.java
@@ -0,0 +1,16 @@
+package com.example.xiaozhou;
+
+public class Inte {
+    public static void f(){
+        System.out.println("F");
+    }
+}
+
+abstract class a{
+    public String s;
+    public abstract void ss();
+}
+
+interface b{
+    String s = null;
+}
